560b89519e6eb71f8842b4de7f9fed82dd137f58
Twig | Christoffer <twig_appleblossom@pm.me>
2026-08-30 16:41:58 +0200

    Added functionality to use online / on phone

--- app.js
+++ app.js
@@ -431,3 +431,15 @@ loadSeeds();
 loadToday();
 
 renderToday();
+
+// -----------------------------
+// SERVICE WORKER
+// -----------------------------
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js").catch((error) => {
+      console.error("Service worker registration failed:", error);
+    });
+  });
+}
